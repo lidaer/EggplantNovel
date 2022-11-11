ddc8bd449fb3d6a9bfed6fabcc6dee2c13ee1bfc
fdse <2795656376@qq.com>
2022-11-11 15:57:47 +0800

    包结构创建完成

--- src/main/java/com/sz/eggplantnovel/EggplantNovelApplication.java
+++ src/main/java/com/sz/eggplantnovel/EggplantNovelApplication.java
@@ -3,6 +3,9 @@ package com.sz.eggplantnovel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author sz
+ */
 @SpringBootApplication
 public class EggplantNovelApplication {
 
