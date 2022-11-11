d48565e54f9d54710f25e68615f141343156efa0
fdse <2795656376@qq.com>
2022-11-11 15:29:42 +0800

    start

---
+++ src/main/java/com/sz/eggplantnovel/EggplantNovelApplication.java
@@ -0,0 +1,13 @@
+package com.sz.eggplantnovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggplantNovelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EggplantNovelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sz/eggplantnovel/EggplantNovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sz.eggplantnovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EggplantNovelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
