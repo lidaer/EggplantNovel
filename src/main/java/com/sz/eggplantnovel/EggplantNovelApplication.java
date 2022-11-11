ae8aec1461cea3a686b24291b140599dc9d891a1
fdse <2795656376@qq.com>
2022-11-11 18:59:31 +0800

    111

--- src/main/java/com/sz/eggplantnovel/EggplantNovelApplication.java
+++ src/main/java/com/sz/eggplantnovel/EggplantNovelApplication.java
@@ -2,12 +2,14 @@ package com.sz.eggplantnovel;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * @author sz
  */
+@SpringBootApplication
 @EnableCaching
 @EnableConfigurationProperties
 @MapperScan("com.sz.eggplantnovel.dao.mapper")
