7428e0f4993ea5049ec0edded93c855520fbdceb
fdse <2795656376@qq.com>
2022-12-04 22:31:45 +0800

    增加json序列化配置，大整数防止精度丢失

--- src/main/java/com/sz/eggplantnovel/dto/resp/HomeFriendLinkRespDto.java
+++ src/main/java/com/sz/eggplantnovel/dto/resp/HomeFriendLinkRespDto.java
@@ -3,13 +3,15 @@ package com.sz.eggplantnovel.dto.resp;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.io.Serializable;
+
 /**
  * @author sz
  * @version 1.0
  * @date 2022/11/14
  */
 @Data
-public class HomeFriendLinkRespDto {
+public class HomeFriendLinkRespDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
